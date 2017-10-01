0074be3168e2c250a234dee96b4a8338d73a523c
jcooperdevlin <jcooperdevlin@ufl.edu>
2017-10-01 12:54:27 -0400

    wham_10.1

--- wham_v1/file_conversion_scripts/humann_2_wham.py
+++ wham_v1/file_conversion_scripts/humann_2_wham.py
@@ -36,7 +36,7 @@ for line in f_in.readlines():
     spline = line.split('\r')
     header = spline[0]
     if "#" in header:
-        print('Acc' + '\t' + "Gene_Family" + '\t' + "Species" + header[13:len(header)-1])
+        print('Acc' + '\t' + "Gene_Family" + '\t' + "Species" + header[13:len(header)])
         samp_list = header.split('\t')
         sample_num = len(samp_list)-1
     if "|" in line:
@@ -46,22 +46,24 @@ for line in f_in.readlines():
         expr_list = ','.join(expr)
         expr_vals1 = expr_list.replace(',', '\t')
         expr_vals = expr_vals1.replace('\n', '')
-        if "unclassified" not in line:
-            if "unknown" not in line:
-                R=re.search('(.*)\_(.*)\:(.*)\|(.*)', heading)
-                acc=R.group(2)
-                gfam=R.group(3)
-                taxa=R.group(4)
-                print(acc + '\t' + gfam + '\t' + taxa + '\t' + expr_vals)
-        else:
-            if "unknown" not in line:
-                things = line.split('\t')
-                heading = things[0]
-                R=re.search('(.*)\_(.*)\:(.*)\|(.*)', heading)
-                acc=R.group(2)
-                gfam=R.group(3)
-                taxa=R.group(4)
-                print(acc + '\t' + gfam + '\t' + taxa + '\t' + expr_vals)
-            gfam_list.append(gfam)
+        expr_vals = expr_vals[0:len(expr_vals)-1]
+        if "NO_NAME" not in line:
+          if "unclassified" not in line:
+              if "unknown" not in line:
+                  R=re.search('(.*)\_(.*)\:(.*)\|(.*)', heading)
+                  acc=R.group(2)
+                  gfam=R.group(3)
+                  taxa=R.group(4)
+                  print(acc + '\t' + gfam + '\t' + taxa + '\t' + expr_vals)
+          else:
+              if "unknown" not in line:
+                  things = line.split('\t')
+                  heading = things[0]
+                  R=re.search('(.*)\_(.*)\:(.*)\|(.*)', heading)
+                  acc=R.group(2)
+                  gfam=R.group(3)
+                  taxa=R.group(4)
+                  print(acc + '\t' + gfam + '\t' + taxa + '\t' + expr_vals)
+            #gfam_list.append(gfam)
 
 f_in.close()
